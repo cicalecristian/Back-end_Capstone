93849e14b29cabf07c03df50b8cac215244cad0d
cicalecristian <cicalecristian@gmail.com>
2026-05-12 12:37:43 +0200

    config email created

---
+++ src/main/java/cristiancicale/capstone/config/EmailSender.java
@@ -0,0 +1,47 @@
+package cristiancicale.capstone.config;
+
+import cristiancicale.capstone.entities.User;
+import kong.unirest.core.HttpResponse;
+import kong.unirest.core.JsonNode;
+import kong.unirest.core.Unirest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailSender {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailSender.class);
+
+    private final String domainName;
+    private final String apiKey;
+
+    public EmailSender(@Value("${mailgun.domain-name}") String domainName, @Value("${mailgun.api-key}") String apiKey) {
+        this.domainName = domainName;
+        this.apiKey = apiKey;
+    }
+
+    public void sendRegistrationEmail(User recipient) {
+
+        try {
+            HttpResponse<JsonNode> response = Unirest.post(
+                            "https://api.mailgun.net/v3/" + domainName + "/messages")
+                    .basicAuth("api", apiKey)
+                    .field("from", "Just Music <cicalecristian@gmail.com>")
+                    .field("to", recipient.getEmail())
+                    .field("subject", "Benvenuto sulla piattaforma Just Music!")
+                    .field("text",
+                            "Ciao " + recipient.getName() + ", la tua registrazione è andata a buon fine!")
+                    .asJson();
+
+            if (response.getStatus() >= 200 && response.getStatus() < 300) {
+                logger.info("Email inviata correttamente");
+            } else {
+                logger.error("Errore invio email: {}", response.getBody());
+            }
+        } catch (Exception e) {
+            logger.error("Errore durante invio email", e);
+        }
+    }
+}
