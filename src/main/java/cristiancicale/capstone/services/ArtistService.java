d007ae43151352d2f4de7834075aaed7d9331a0f
cicalecristian <cicalecristian@gmail.com>
2026-05-12 16:05:40 +0200

    services Artist and Song created

---
+++ src/main/java/cristiancicale/capstone/services/ArtistService.java
@@ -0,0 +1,62 @@
+package cristiancicale.capstone.services;
+
+import cristiancicale.capstone.entities.Artist;
+import cristiancicale.capstone.exceptions.NotFoundException;
+import cristiancicale.capstone.payloads.ArtistDTO;
+import cristiancicale.capstone.repositories.ArtistRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class ArtistService {
+
+    private final ArtistRepository artistRepository;
+
+
+    public ArtistService(ArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+
+    public Artist save(ArtistDTO body) {
+
+        Artist artist = new Artist(body.artistName(), body.nationality(), body.dateOfBirth(), body.genre(), body.avatar());
+
+        return artistRepository.save(artist);
+    }
+
+    public Page<Artist> findAll(int page, int size, String sortBy) {
+        if (size > 10 || size < 0) size = 10;
+        if (page < 10) page = 0;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return this.artistRepository.findAll(pageable);
+    }
+
+    public Artist findById(UUID id) {
+        return artistRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    public Artist findByIdAndUpdate(UUID id, ArtistDTO body) {
+
+        Artist found = this.findById(id);
+
+        found.setArtistName(body.artistName());
+        found.setNationality(body.nationality());
+        found.setDateOfBirth(body.dateOfBirth());
+        found.setGenre(body.genre());
+        found.setAvatar(body.avatar());
+
+        return artistRepository.save(found);
+    }
+
+    public void findByIdAndDelete(UUID id) {
+        Artist found = this.findById(id);
+        artistRepository.delete(found);
+    }
+}
---
+++ src/main/java/cristiancicale/capstone/services/SongService.java
@@ -0,0 +1,87 @@
+package cristiancicale.capstone.services;
+
+import cristiancicale.capstone.entities.Artist;
+import cristiancicale.capstone.entities.Song;
+import cristiancicale.capstone.entities.SongArtist;
+import cristiancicale.capstone.exceptions.NotFoundException;
+import cristiancicale.capstone.payloads.SongArtistDTO;
+import cristiancicale.capstone.payloads.SongDTO;
+import cristiancicale.capstone.repositories.SongRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class SongService {
+
+    private final SongRepository songRepository;
+    private final ArtistService artistService;
+
+    public SongService(SongRepository songRepository, ArtistService artistService) {
+        this.songRepository = songRepository;
+        this.artistService = artistService;
+    }
+
+    public Song save(SongDTO body) {
+
+        Song song = new Song(body.title(), body.cover(), body.duration(), body.releaseDate());
+
+        return songRepository.save(song);
+    }
+
+    public Page<Song> findAll(int page, int size, String sortBy) {
+        if (size > 10 || size < 0) size = 10;
+        if (page < 10) page = 0;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return this.songRepository.findAll(pageable);
+    }
+
+    public Song findById(UUID id) {
+        return songRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    public Song findByIdAndUpdate(UUID id, SongDTO body) {
+
+        Song found = findById(id);
+
+        found.setTitle(body.title());
+        found.setCover(body.cover());
+        found.setDuration(body.duration());
+        found.setGenre(body.genre());
+        found.setReleaseDate(body.releaseDate());
+
+        found.getSongArtists().clear();
+
+        Set<SongArtist> artists = new HashSet<>();
+
+        for (SongArtistDTO artistDTO : body.artists()) {
+
+            Artist artist = artistService.findById(artistDTO.artistId());
+
+            SongArtist songArtist = new SongArtist(
+                    artistDTO.role(),
+                    found,
+                    artist
+            );
+
+            artists.add(songArtist);
+        }
+
+        found.setSongArtists(artists);
+
+        return songRepository.save(found);
+    }
+
+    public void findByIdAndDelete(UUID id) {
+        Song found = this.findById(id);
+        songRepository.delete(found);
+    }
+}
