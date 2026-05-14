fdc656db2ab0bcb21144fcef6e0bab30bef396d7
cicalecristian <cicalecristian@gmail.com>
2026-05-14 16:19:08 +0200

    created SaveUser for controller

--- src/main/java/cristiancicale/capstone/controllers/UserController.java
+++ src/main/java/cristiancicale/capstone/controllers/UserController.java
@@ -2,15 +2,20 @@ package cristiancicale.capstone.controllers;
 
 import cristiancicale.capstone.entities.User;
 import cristiancicale.capstone.enums.RoleUser;
+import cristiancicale.capstone.exceptions.ValidationException;
 import cristiancicale.capstone.payloads.UserDTO;
+import cristiancicale.capstone.payloads.UserRespDTO;
 import cristiancicale.capstone.services.UserService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,6 +26,19 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserRespDTO saveUser(@RequestBody @Validated UserDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            List<String> errors = validationResult.getFieldErrors().stream().map(error -> error.getDefaultMessage()).toList();
+            throw new ValidationException(errors);
+        }
+
+        User newUser = this.userService.save(body);
+        return new UserRespDTO(newUser.getId(), newUser.getUsername(), newUser.getEmail(), newUser.getName(),
+                newUser.getSurname(), newUser.getDateOfBirth(), newUser.getRole(), newUser.getAvatar());
     }
 
     @GetMapping
