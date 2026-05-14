672c9bfebfc38bf7d16006d5e259387654171e28
cicalecristian <cicalecristian@gmail.com>
2026-05-14 19:19:52 +0200

    new covers created

--- src/main/java/cristiancicale/capstone/exceptions/ErrorsHandler.java
+++ src/main/java/cristiancicale/capstone/exceptions/ErrorsHandler.java
@@ -3,6 +3,7 @@ package cristiancicale.capstone.exceptions;
 import cristiancicale.capstone.payloads.ErrorsDTO;
 import cristiancicale.capstone.payloads.ErrorsListDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
